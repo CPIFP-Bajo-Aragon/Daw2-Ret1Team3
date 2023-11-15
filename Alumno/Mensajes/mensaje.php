3ce719affec6b071d216d81550ea21d723fa080c
Oscar Blasco <84106229+Oscarr36@users.noreply.github.com>
2023-11-15 20:16:40 +0100

    Actualizacion enviar mensajes

--- Alumno/Mensajes/mensaje.php
+++ Alumno/Mensajes/mensaje.php
@@ -4,12 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mensajes</title>
+    <link rel="stylesheet" href="../../Estilos/alumno.css">
+
 </head>
 <body>
-    <form action="mensaje.php" method="post">
-        <input type="text" name="dni_usuario" id="dni_usuario" placeholder="Pon el DNI">
-        <input type="submit" value="Iniciar chat">
-    </form>
+        <main>
+            <header class="main-header">
+                <img src="../../Imagenes/Profitech.png" alt="">
+                <div class="conInfo">
+                    <div class="conInfo">
+                        <p>Hola, // echo $username</p>
+                        <form action="cerrarSesion.php" method="post">
+                            <input type="submit" value="Cerrar sesión" />
+                        </form>
+                    </div>
+            </header>
+            <div class="main-content">
+                <nav class="main-menu">
+                    <ul>
+                        <a href="#">
+                            <li>Inicio</li>
+                        </a>
+                        <a href="#">
+                            <li>Curriculum</li>
+                        </a>
+                        <a href="../Alertas/index.php">
+                            <li>Mis alertas</li>
+                        </a>
+                        <a href="#">
+                            <li>Mensajes</li>
+                        </a>
+                        <a href="../Mis_Ofertas/ofertas.php">
+                            <li>Mis ofertas</li>
+                        </a>
+                        <hr>
+                        <a href="/../Alumno/Buscar_Empresas/index.php">
+                            <li>Buscar empresas</li>
+                        </a>
+                        <a href="/../Alumno/Buscar_Ofertas/index.php">
+                            <li>Buscar ofertas</li>
+                        </a>
+                        <hr>
+                        <a href="../../Cambiar_Clave/Alumno/Cambiar_Clave_Alumno.php">
+                            <li>Cambiar contraseña</li>
+                        </a>
+
+                    </ul>
+                </nav>
+                <section class="main-info">
+                    <article class="card">
+
+
+  <p>Chats Activos actualmente</p>
     <!-- Vamos a recoger el dni del emisor  -->
     <?php 
         session_start();
@@ -23,9 +69,41 @@
         $dni_destino=$_SESSION['dni_usuario_mensaje'];
         
          
+    $chats_participado = "SELECT Origen_Mensaje, Destino_Mensaje  FROM bolsa_emplea.mensaje where Destino_Mensaje='$dni_origen' group by Origen_Mensaje";
+    if ($resultado_mensaje = $conexion->query($chats_participado)) {
+       while ($row = $resultado_mensaje->fetch(PDO::FETCH_OBJ)) {
+           $Origen_DNI_Chat = $row->Origen_Mensaje;
+           $Destino_DNI_Chat = $row->Destino_Mensaje;
+           ?>
+           <form action="mensaje.php" method="post">
+               <input type="hidden" value="<?php echo $Origen_DNI_Chat ?>" name="dni_usuario" id="dni_usuario">
+               <?php
+               $sql_nombre = "SELECT Nombre_Usuario FROM `Usuario` WHERE DNI_CIF='$Origen_DNI_Chat'";
+               $resultado_nombre = $conexion->query($sql_nombre);
+       
+               while ($row_nombre = $resultado_nombre->fetch(PDO::FETCH_OBJ)) {
+                   $Nombre = $row_nombre->Nombre_Usuario;
+               }
+               ?>
+               <input type="submit" value="<?php echo $Nombre ?>">
+           </form>
+           <?php
+       }
+   }
 
 
 
+
+?>
+<p>Inicia un nuevo chat</p>
+  <form action="mensaje.php" method="post">
+        <input type="text" name="dni_usuario" id="dni_usuario" placeholder="Pon el DNI">
+        <input type="submit" value="Iniciar chat">
+    </form>
+<hr>
+<p>Mensajes:</p>
+<br>
+    <?php
 
         $sql_carga_mensaje = "SELECT * FROM `Mensaje` WHERE (Origen_Mensaje = '$dni_origen' OR  Origen_Mensaje = '$dni_destino')AND (Destino_Mensaje= '$dni_destino' or Destino_Mensaje= '$dni_origen') ORDER BY ID_Mensaje ASC  ";
 
@@ -53,5 +131,11 @@ if ($resultado_mensaje = $conexion->query($sql_carga_mensaje)) {
         <input type="text" name="mensaje" id="mensaje">
         <input type="submit" value="Enviar mensaje">
     </form>
+
+
+    </article>
+
+</section>
 </body>
+
 </html>
